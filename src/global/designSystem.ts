838ed8f2981b5f453cdb99f2b0622135ff98d815
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-02 10:17:58 -0600

    Icons component created

---
+++ src/global/designSystem.ts
@@ -0,0 +1,10 @@
+export const colorSystem = {
+	darkestBackground: '#0C1821',
+	darkerBackground: '#1b2a41',
+	background: '#26375a',
+	blue: '#56ccf2',
+	yellow: '#f2c94c',
+	desaturatedYellow: '#ead38d',
+	orange: '#eb5757',
+	green: '#27ae60',
+};
