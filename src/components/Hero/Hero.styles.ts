2fa054ed5b504c7771b1d6a7d7d499573704660a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-03 08:36:39 -0600

    Panels on mobile completed

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
+import { borderSystem } from '../../global/designSystem';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+	.hero {
+		&__image {
+			position: relative;
+			height: 800px;
+			margin: 0 auto;
+			width: 320px;
+			&__first-panel {
+				position: absolute;
+				padding: 16px;
+				max-width: 300px;
+				border-radius: ${borderSystem.normal};
+				background: linear-gradient(
+					180deg,
+					#0c1821 0%,
+					rgba(12, 24, 33, 0.39) 90.1%
+				);
+				box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+				& > p {
+					font-size: 1.4rem;
+				}
+			}
+			&__second-panel {
+				position: absolute;
+				top: 280px;
+				left: 80px;
+				padding: 16px;
+				max-width: 236px;
+				border-radius: ${borderSystem.normal};
+				background: linear-gradient(
+					180deg,
+					rgba(12, 24, 33, 0.56) 0%,
+					rgba(12, 24, 33, 0.39) 100%
+				);
+				box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+				& > p {
+					font-size: 1.4rem;
+				}
+			}
+		}
+	}
+`;
 
 export default Styles;
